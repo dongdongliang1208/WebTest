6131ebe5fd8b2292c361b54bce1772b19fcdb70d
dongdl <dongdl@chingo.cn>
2018-05-24 18:59:38 +0800

    Initial commit

---
+++ src/net/test/servlet/WebServlet.java
@@ -0,0 +1,18 @@
+package net.test.servlet;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class WebServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        System.out.println(Arrays.toString(req.getParameterValues("week"))+123);
+        resp.sendRedirect("/index.jsp");
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dongdl
+  Date: 2018/4/11
+  Time: 15:17
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>$Title$</title>
+</head>
+<body>
+$END$
+<form action="/WebServlet" method="POST">
+    <input type="checkbox" name="week" value="选择1"/>选择1
+    <input type="checkbox" name="week" value="选择2"/>选择2
+    <input type="checkbox" name="week" value="选择3"/>选择3
+    <input type="submit"/>
+</form>
+</body>
+</html>
